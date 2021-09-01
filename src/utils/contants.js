f905c7b671ed031f8425e786830e7c8c82bc9a08
abnerpersio <binhopersio@gmail.com>
2021-08-31 22:44:44 -0300

    feat: adds online server url

--- src/utils/contants.js
+++ src/utils/contants.js
@@ -1,2 +1,3 @@
 // export const API_URL = process.env.API_URL || 'http://localhost:8080';
 // export const API_URL = 'http://10.0.0.190:8080';
+export const API_URL = 'https://web-agenda-api.herokuapp.com';
